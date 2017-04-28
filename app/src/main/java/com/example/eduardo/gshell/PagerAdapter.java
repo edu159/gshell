f113f376472ba167b850fcea79a1886a963f1a79
jgolebiowski <jacek.golebiowski14@imprial.ac.uk>
2017-04-28 15:15:37 +0100

    minor change

--- app/src/main/java/com/example/eduardo/gshell/PagerAdapter.java
+++ app/src/main/java/com/example/eduardo/gshell/PagerAdapter.java
@@ -23,7 +23,7 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
                 TabFragment1 tab1 = new TabFragment1(this.server);
                 return tab1;
             case 1:
-                TabFragment2 tab2 = new TabFragment2();
+                TabFragment2 tab2 = new TabFragment2(this.server);
                 return tab2;
             case 2:
                 TabFragment3 tab3 = new TabFragment3();
